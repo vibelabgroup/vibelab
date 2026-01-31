383609ea281dc3e06d43b5d88710f24a6be22ed8
vibelabgroup <vibelabgroup@gmail.com>
2026-01-31 16:21:37 +0100

    Update App.tsx

--- App.tsx
+++ App.tsx
@@ -1,11 +1,11 @@
 import React from 'react';
-import Navigation from './components/Navigation.tsx';
-import Hero from './components/Hero.tsx';
-import Manifesto from './components/Manifesto.tsx';
-import Expertise from './components/Expertise.tsx';
-import CallToAction from './components/CallToAction.tsx';
-import Footer from './components/Footer.tsx';
-import CustomCursor from './components/CustomCursor.tsx';
+import Navigation from './components/Navigation';
+import Hero from './components/Hero';
+import Manifesto from './components/Manifesto';
+import Expertise from './components/Expertise';
+import CallToAction from './components/CallToAction';
+import Footer from './components/Footer';
+import CustomCursor from './components/CustomCursor';
 
 const App: React.FC = () => {
   return (
